118850b8b13be8397d4505e9ce2c6816ad11f6e7
Benjamin Chan <benjamin.ks.chan@gmail.com>
2019-05-23 07:25:39 -0700

    Set root directory

--- scripts/make.R
+++ scripts/make.R
@@ -1,6 +1,7 @@
 library("knitr")
 library("rmarkdown")
 opts_chunk$set(fig.path = "figures/")
+opts_knit$set(root.dir = getwd())
 knit("scripts/BDP2-448.Rmd", output = "docs/BDP2-448.md")
 pandoc("docs/BDP2-448.md", format = "docx")
 # system("pandoc --template=GitHub.html5 --self-contained docs/BDP2-448.md -o docs/BDP2-448.html")
